a48bd15c45259d0bdf18088d9b2c6df7f5033f84
rafakka <rafaelkaher@gmail.com>
2026-02-05 11:28:42 -0300

    jest installed for easier testing

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+
+const config = {
+    preset:"ts-jest",
+    testEnvironment:"node",
+    testMatch:["**/*.test.ts"],
+    testTimeout:30000,
+    forceExit:true,
+    moduleFileExtensions:["ts","js"],
+    rootDir:"",
+}
+
+export default config
