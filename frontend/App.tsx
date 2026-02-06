c97c002b40996e5f15d0d33dbc76869464850319
rafakka <rafaelkaher@gmail.com>
2026-02-06 13:30:51 -0300

    trying to set up react+tailwind

---
+++ frontend/App.tsx
@@ -0,0 +1,11 @@
+
+
+export default function App(){
+    return (
+        <div className="h-screen flext items-center justify-center bg-red-100">
+            <h1 className="text-4xl font-bold text-red-600">
+                POINT AUDIT
+            </h1>
+        </div>
+        );
+    }
