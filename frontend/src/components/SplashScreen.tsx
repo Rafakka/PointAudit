fabb3f17ea801f1272148bec4c78a1471f886c5c
rafakka <rafaelkaher@gmail.com>
2026-02-09 08:50:40 -0300

    first page loading draft done

--- frontend/src/components/SplashScreen.tsx
+++ frontend/src/components/SplashScreen.tsx
@@ -1,36 +1,32 @@
 
-type SplashScreenProps = {
-    progress:number;
-};
+function LoadingDots(){
+   return (
+    <div className="loading-dots-container">
+        <div className="loading-dot"/>
+        <div className="loading-dot"/>
+        <div className="loading-dot"/>
+        </div>
+   );
+}
 
-export default function SplashScreen({progress}:SplashScreenProps) {
-    const totalDots = 8
-    const activeDots = Math.round((progress/100)*totalDots)
 
+export default function SplashScreen({progress}:
+    {progress:number}
+) {
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center bg-white">
-            {/*logo*/}
             <img
             src="/logo.png"
-            alt="Point audit"
-            className="w-40 mb-6 animate-fade-in"
+            alt="Point Audit"
+            className="w-40 mb-6 block mx-auto"
             />
-            {/*title*/}
-            <h1 className="text-3xl font-bold text-red-600 mb-2 animate-fade-in-delay">
-                POINT AUDIT
+            <h1 className="text-3xl font-bold text-red-600, mb-2">
+                Bem-vindo
             </h1>
-            {/*loading text*/}
-            <div className="flex gap-2">
-                {Array.from({length:totalDots}).map((__,i)=>(
-                    <span
-                    key={i}
-                    className={`w-3 h-3 rounded-full transition-opacity duration-300 ${
-                        i < activeDots
-                        ? "bg-cyan-100" :"bg-cyan-20"
-                    }`}
-                    />
-                ))}
-            </div>
+            <p className="text-gray-500 mb-4">
+                Carregando            
+            </p>
+            <LoadingDots/>
         </div>
     );
 }
